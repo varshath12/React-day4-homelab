74f01e45c78483a53312686a4ef2102f519b7c25
varshath12 <108916881+varshath12@users.noreply.github.com>
2023-03-29 12:15:40 +0530

    Add files via upload

---
+++ App (6).js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './App.css';
+import D4PAH from './D4PAH';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <D4PAH/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
